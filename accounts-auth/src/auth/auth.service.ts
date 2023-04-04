4776f513d898b0d0ecbb576f475a62789ec76bb1
enzomoraes <enzomoraes12@hotmail.com>
2023-04-04 14:57:36 -0400

    feat: removing authentication

--- accounts-auth/src/auth/auth.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AuthService {
-  constructor() {}
-
-  async login(username: string, password: string): Promise<any> {}
-}
--- accounts-auth/src/users/users.controller.ts
+++ accounts-auth/src/users/users.controller.ts
@@ -15,7 +15,6 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserValidatorPipe } from '../pipes/create-user-validator/create-user-validator.pipe';
 import { UserMapper } from './dto/mapper';
-import { JwtGuard } from '../auth/jwt.guard';
 
 @Controller('users')
 export class UsersController {
@@ -23,7 +22,6 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @UseGuards(JwtGuard)
   @UsePipes(CreateUserValidatorPipe)
   async create(@Body() createUserDto: CreateUserDto) {
     return this.mapper.toResponse(
@@ -32,7 +30,6 @@ export class UsersController {
   }
 
   @Get()
-  @UseGuards(JwtGuard)
   async findAll(@Query('orderDirection') orderDirection: -1 | 1 = -1) {
     return this.mapper.toResponseList(
       await this.usersService.findAll(orderDirection),
@@ -40,13 +37,11 @@ export class UsersController {
   }
 
   @Get(':id')
-  @UseGuards(JwtGuard)
   async findOne(@Param('id') id: string) {
     return this.mapper.toResponse(await this.usersService.findOne(id));
   }
 
   @Patch(':id')
-  @UseGuards(JwtGuard)
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.mapper.toResponse(
       await this.usersService.update(id, updateUserDto),
@@ -54,7 +49,6 @@ export class UsersController {
   }
 
   @Delete(':id')
-  @UseGuards(JwtGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
