ec193dad234886f9bba8c3a239acd2828e96a8b9
enzomoraes <enzomoraes12@hotmail.com>
2023-04-04 11:48:48 -0400

    feat: adding same login validation

--- accounts-auth/src/pipes/create-user-validator/create-user-validator.pipe.spec.ts
+++ accounts-auth/src/pipes/create-user-validator/create-user-validator.pipe.spec.ts
@@ -1,41 +1,99 @@
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import CreateUserException from '../../users/exceptions/CreateUserException';
 import { CreateUserValidatorPipe } from './create-user-validator.pipe';
+import { UserEntity, UserSchema } from '../../users/entities/user.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import Mongoose, { Model } from 'mongoose';
+import { config } from 'dotenv';
+config();
 
 describe('CreateUserValidatorPipe', () => {
+  let userModel: Model<UserEntity>;
+  let module: TestingModule;
+  let pipe: CreateUserValidatorPipe;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(
+          `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`,
+          {
+            dbName: 'testes',
+          },
+        ),
+        MongooseModule.forFeature([
+          { schema: UserSchema, name: UserEntity.name },
+        ]),
+      ],
+      providers: [CreateUserValidatorPipe],
+    }).compile();
+
+    userModel = module.get(getModelToken(UserEntity.name));
+    pipe = module.get(CreateUserValidatorPipe);
+  });
+
+  afterEach(async () => {
+    await userModel.deleteMany();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
   it('should be defined', () => {
-    expect(new CreateUserValidatorPipe()).toBeDefined();
+    expect(pipe).toBeDefined();
   });
 
   it('should throw when login null', () => {
-    const validator = new CreateUserValidatorPipe();
 
-    expect(() =>
-      validator.transform(
-        { login: null, name: 'teste', timestamp: Number(new Date()) },
-        { type: 'body' },
-      ),
-    ).toThrow(new CreateUserException('login is required'));
+    expect(
+      async () =>
+        await pipe.transform(
+          { login: null, name: 'teste', timestamp: Number(new Date()) },
+          { type: 'body' },
+        ),
+    ).rejects.toThrow(new CreateUserException('login is required'));
   });
 
   it('should throw when name null', () => {
-    const validator = new CreateUserValidatorPipe();
 
-    expect(() =>
-      validator.transform(
+    expect(async () =>
+      await pipe.transform(
         { login: 'teste', name: null, timestamp: Number(new Date()) },
         { type: 'body' },
       ),
-    ).toThrowError(new CreateUserException('name is required'));
+    ).rejects.toThrowError(new CreateUserException('name is required'));
   });
 
   it('should throw when timestamp null', () => {
-    const validator = new CreateUserValidatorPipe();
 
-    expect(() =>
-      validator.transform(
+    expect(async () =>
+      await pipe.transform(
         { login: 'teste', name: 'teste', timestamp: null },
         { type: 'body' },
       ),
-    ).toThrowError(new CreateUserException('timestamp is required'));
+    ).rejects.toThrowError(new CreateUserException('timestamp is required'));
+  });
+
+  it('should throw when login already exists', async () => {
+
+    await new userModel({
+      _id: new Mongoose.Types.ObjectId(),
+      login: 'login',
+      name: 'name',
+      timestamp: Number(new Date()),
+      password: 'pass',
+    }).save();
+
+    expect(async () =>
+      await pipe.transform(
+        { login: 'login', name: 'teste', timestamp: Number(new Date()) },
+        { type: 'body' },
+      ),
+    ).rejects.toThrowError(
+      new CreateUserException(
+        'This login is already taken! Please, choose another one.',
+      ),
+    );
   });
 });
--- accounts-auth/src/pipes/create-user-validator/create-user-validator.pipe.ts
+++ accounts-auth/src/pipes/create-user-validator/create-user-validator.pipe.ts
@@ -1,18 +1,25 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import { CreateUserDto } from '../../users/dto/create-user.dto';
 import CreateUserException from '../../users/exceptions/CreateUserException';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserEntity } from '../../users/entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CreateUserValidatorPipe
-  implements PipeTransform<CreateUserDto, CreateUserDto>
+  implements PipeTransform<CreateUserDto, Promise<CreateUserDto>>
 {
+  constructor(
+    @InjectModel(UserEntity.name) private userModel: Model<UserEntity>,
+  ) {}
+
   /**
    * This class validates log creation
    * @param value
    * @param metadata
    * @returns
    */
-  transform(value: CreateUserDto, metadata: ArgumentMetadata) {
+  async transform(value: CreateUserDto, metadata: ArgumentMetadata) {
     if (!value.login) {
       throw new CreateUserException('login is required');
     }
@@ -23,6 +30,16 @@ export class CreateUserValidatorPipe
 
     if (!value.timestamp) {
       throw new CreateUserException('timestamp is required');
+    }
+
+    const existingUser: UserEntity = await this.userModel
+      .findOne({ login: value.login })
+      .exec();
+    
+    if (existingUser) {
+      throw new CreateUserException(
+        'This login is already taken! Please, choose another one.',
+      );
     }
 
     return value;
