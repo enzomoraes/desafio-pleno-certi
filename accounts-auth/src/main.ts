40541aaba2ba24c78f3a36239a0ed7a43fbebcb5
enzomoraes <enzomoraes12@hotmail.com>
2023-04-05 18:29:22 -0400

    chore: cors

--- accounts-auth/src/main.ts
+++ accounts-auth/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.connectMicroservice(microserviceConfig);
+  app.enableCors({origin: '*'})
 
   await app.startAllMicroservices();
   await app.listen(3000);
--- logger/src/main.ts
+++ logger/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.connectMicroservice(microserviceConfig);
-
+  app.enableCors({ origin: '*' });
+  
   await app.startAllMicroservices();
   await app.listen(3001);
 }
